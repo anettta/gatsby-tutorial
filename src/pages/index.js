d37a4511285a6332f693108431d1cf97f2359d14
Anya <annagapyuk@gmail.com>
2021-02-17 17:48:15 -0500

    created navbar and footer components

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {Link} from 'gatsby'
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+export default function Home() {
+  return <div>
+     <Navbar />
+     <div>
+  Hello!
+     </div>
+   
+    
+
+<Footer />
+  </div>
+}
